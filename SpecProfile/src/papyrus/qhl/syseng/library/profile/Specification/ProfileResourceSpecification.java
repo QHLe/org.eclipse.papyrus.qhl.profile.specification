b490818fce3fe3cc656bb92f68a78b586cd4edb1
zineus <zineus@web.de>
2019-03-21 22:26:39 +0100

    Profile Resource Class added

---
+++ SpecProfile/src/papyrus/qhl/syseng/library/profile/Specification/ProfileResourceSpecification.java
@@ -0,0 +1,9 @@
+package papyrus.qhl.syseng.library.profile.Specification;
+
+public class ProfileResourceSpecification {
+	
+	public static final String PROFILES_PATHMAP = "pathmap:/model/"; //$NON-NLS-1$	
+	
+	public static final String PROFILE_PATH = PROFILES_PATHMAP+"Specification.profile.uml"; //$NON-NLS-1$
+	
+}
